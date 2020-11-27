7fe3beab30c840b287edf73f105d3b67bcc22f6f
raddanfea <radidani55@gmail.com>
2020-11-27 21:06:48 +0100

    cleared file cache

--- pythonProject/beadandó/youtube-dl-best.py
+++ pythonProject/beadandó/youtube-dl-best.py
@@ -1,26 +1,35 @@
+#!/usr/bin/env python3
+
 import subprocess
 import sys
+import os
 
 
 def printHelp():
     HELP = """
+    USAGE: ./youtube-dl-best [URL] [extra arg]
     This is a python script that help you download the best video quality using youtube_dl.
     To merge the file formats, you need ffmpeg or avconv installed.
     For this script to run, you have to be able to run youtube-dl from terminal.
-    -h Prints help block.
-    --webm Convert it to webm instead of the default mp4.
-    --nomix Don't mix different video and audio formats. Easier to process.
+    Your first argument MUST be a link, additional arguments may be in any order after that.
+    You can use youtube_dl arguments.
+    
+    Warning: ffmpeg often fails combining different formats. Use --nomix if needed!
+    
+    -h          Prints this help block.
+    --help      Prints youtube_dl's help.
+    --webm      Convert it to webm instead of the default mp4.
+    --nomix     Don't mix different video and audio formats. Easier to process.
     """.strip()
-
     print(HELP)
 
 
 def getLink(args):
-    lnk = ''
-    for test in args:
-        if 'youtu' in test:
-            lnk = test
-    return lnk
+    if len(args) > 1:
+        return args[1]
+    else:
+        print("No link?")
+        return None
 
 
 def getTypes(link):
@@ -29,6 +38,13 @@ def getTypes(link):
     resp = proc.communicate()[0]
     proc.terminate()
     return resp
+
+
+def printYtDlHelp():
+    proc = subprocess.Popen("youtube-dl --help ", stdout=sys.stdout,
+                            stderr=sys.stderr, shell=True)
+    resp = proc.communicate()[0]  # Do NOT delete this or you get no response from proc.
+    proc.terminate()
 
 
 class VideoOptions:
@@ -94,45 +110,62 @@ def getBestOO(response, ext, mix):
     return bestVideoOption.getId(), bestVideoOption.getInfo(), bestAudioOption.getId(), bestAudioOption.getInfo()
 
 
-def downloadBest(bestV, bestA, link, ext):
-    proc = subprocess.Popen("youtube-dl -f " + bestV + '+' + bestA + ' ' + link + ' ' + '--merge-output-format ' + ext,
+def downloadBest(bestV, bestA, link, ext, extraArgs):
+    proc = subprocess.Popen("youtube-dl -f " + bestV + '+' + bestA + ' ' + link + ' '
+                            + '--merge-output-format ' + ext
+                            + extraArgs,
                             stdout=sys.stdout,
-                            stderr=subprocess.STDOUT, shell=True)
+                            stderr=sys.stdout, shell=True)
     resp = proc.communicate()[0]
-    proc.terminate()
+
+
+def parseArgs(args):
+    buffer = [' ' + str(x) for x in args]
+    return "".join(buffer)
 
 
 def main():
-    link = None
     args = sys.argv
     ext = 'mp4'
     mix = True
-    request = ''
-
-    if '-h' in args:
-        printHelp()
-    else:
-        link = getLink(args)
 
     if '--webm' in args:
         ext = 'webm'
+        args.remove('--webm')
 
     if '--nomix' in args:
         mix = False
+        args.remove('--nomix')
 
+    if '--help' in args:
+        printYtDlHelp()
+        args.remove('--help')
+        sys.exit(0)
+
+    if '-h' in args:
+        printHelp()
+        args.remove('-h')
+        sys.exit(0)
+
+    link = getLink(args)
     if link is not None:
         response = getTypes(link)
 
-        if response.decode('utf-8').startswith('ERROR'):
+        # removing unnecessary data to prepare for passing extra arguments
+        args = args[2:]
+
+        if response.decode('utf-8').startswith('ERROR') or response.decode('utf-8').startswith('Usage'):
             print("ERROR: Invalid link.", file=sys.stderr)
         else:
+            extraArgs = parseArgs(args)
+
             # I have no idea why, but these get flipped on return.
             bestA, bestAInfo, bestV, bestVInfo = getBestOO(response, ext, mix)
             print('Best Audio:  ' + bestAInfo + '\n' + 'Best Video:  ' + bestVInfo)
             request = input("Download? Y/N \n")
 
-    if request.lower() in ['yes', 'y']:
-        downloadBest(bestV, bestA, link, ext)
+            if request.lower() in ['yes', 'y']:
+                downloadBest(bestV, bestA, link, ext, extraArgs)
 
 
 if __name__ == '__main__':
